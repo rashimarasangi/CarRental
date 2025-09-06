36b3e7735d5bc44bb805c80c46b252ff1224c135
rashimarasangi <rashimarasangi200@gmail.com>
2025-09-06 14:38:42 +0530

    hero section is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { useState } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Cars from './pages/Cars'
+import MyBookings from './pages/MyBookings'
+import Home from './pages/Home'
+import CarDetails from './pages/CarDetails'
 
 const App = () => {
+
+
+  const [showLogin,setShowLogin]= useState(false)
+  const isOwnberPath = useLocation().pathname.startsWith('/owner')
   return (
-    <div>
-     <Navbar/>
-    </div>
+    <>
+     {!isOwnberPath && <Navbar setShowLogin={setShowLogin}/>}
+     <Routes>
+      <Route path ='/' element={<Home/>}/>
+       <Route path ='/car-details/:id' element={<CarDetails/>}/>
+        <Route path ='/cars' element={<Cars/>}/>
+         <Route path ='/my-bookings' element={<MyBookings/>}/>
+     </Routes>
+
+
+
+    </>
   )
 }
 
