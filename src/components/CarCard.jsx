82f4e062cfacf3110bc59ea0d8c5278b606ea962
rashimarasangi <rashimarasangi200@gmail.com>
2025-09-07 08:55:02 +0530

    title section is added

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -16,7 +16,33 @@ const CarCard = ({car}) => {
         <span className='text-sm text-white/80'> / day</span>
         </div>
       </div>
-      <div></div>
+      <div className='p-4 sm:p-5'>
+        <div className='flex justify-between items-start mb-2'>
+            <div>
+                <h3 className='text-lg font-medium'>{car.brand}{car.model}</h3>
+                <p>{car.category} . {car.year}</p>
+            </div>
+        </div>
+        <div className='mt-4 grid grid-cols-2 gap-y-2 text-gray-600'>
+             <div className='flex items-center text-sm text-muted-foreground'>
+              <img src = {assets.users_icon} alt="" className='h-4 mr-2'/>
+              <span>{car.seating_capacity} Seats</span>
+             </div>
+             <div className='flex items-center text-sm text-muted-foreground'>
+              <img src = {assets.fuel_icon} alt="" className='h-4 mr-2'/>
+              <span>{car.fuel_type}</span>
+             </div>
+             <div className='flex items-center text-sm text-muted-foreground'>
+              <img src = {assets.car_icon} alt="" className='h-4 mr-2'/>
+              <span>{car.transmission} Seats</span>
+             </div>
+             <div className='flex items-center text-sm text-muted-foreground'>
+              <img src = {assets.location_icon} alt="" className='h-4 mr-2'/>
+              <span>{car.location} Seats</span>
+             </div>
+        </div>
+
+      </div>
     </div>
   )
 }
