2aa7e24d1a06d9b4c8124d557e8c1160678159d7
rashimarasangi <rashimarasangi200@gmail.com>
2025-09-08 13:23:04 +0530

    footer is added

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,24 @@ import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
 import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
+import Footer from './components/Footer'
 
 const App = () => {
 
 
   const [showLogin,setShowLogin]= useState(false)
-  const isOwnberPath = useLocation().pathname.startsWith('/owner')
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
   return (
     <>
-     {!isOwnberPath && <Navbar setShowLogin={setShowLogin}/>}
+     {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
      <Routes>
       <Route path ='/' element={<Home/>}/>
        <Route path ='/car-details/:id' element={<CarDetails/>}/>
         <Route path ='/cars' element={<Cars/>}/>
          <Route path ='/my-bookings' element={<MyBookings/>}/>
      </Routes>
-
-
+      {!isOwnerPath && <Footer/>}
+     
 
     </>
   )
