5330c21f6adfc63c16a5e3d98a52f6a01e48c3ee
rashimarasangi <rashimarasangi200@gmail.com>
2025-09-10 12:20:04 +0530

    updated app  section is added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ import MyBookings from './pages/MyBookings'
 import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
 import Footer from './components/Footer'
+import Layout from './pages/owner/Layout'
+import Dashboard from './pages/owner/Dashboard'
+import AddCar from './pages/owner/AddCar'
+import ManageCars from './pages/owner/ManageCars'
+import ManageBookings from './pages/owner/ManageBookings'
 
 const App = () => {
 
@@ -21,6 +26,12 @@ const App = () => {
        <Route path ='/car-details/:id' element={<CarDetails/>}/>
         <Route path ='/cars' element={<Cars/>}/>
          <Route path ='/my-bookings' element={<MyBookings/>}/>
+         <Route path='/owner' element={<Layout/>}>
+            <Route index element ={<Dashboard/>}/>
+            <Route path='add-car' element ={<AddCar/>}/>
+            <Route path='manage-cars' element ={<ManageCars/>}/>
+            <Route path='manage-bookings' element ={<ManageBookings/>}/>
+         </Route>
      </Routes>
       {!isOwnerPath && <Footer/>}
      
