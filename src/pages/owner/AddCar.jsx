2144db0cf28772cd1518327d7c7a3a3073ad904c
rashimarasangi <rashimarasangi200@gmail.com>
2025-09-11 18:34:14 +0530

    add car section  is added

--- src/pages/owner/AddCar.jsx
+++ src/pages/owner/AddCar.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { useState } from 'react'
+import Title from '../../components/owner/Title'
+import { assets } from '../../assets/assets'
 
 const AddCar = () => {
+
+  const [image, setImage] = useState(null)
+  const [car, setCar] = useState({
+    brand:'',
+    model:'',
+    year: 0,
+    pricePerDay: 0,
+    category:'',
+    transmission:'',
+    fuel_type: '',
+    seating_capacity: 0,
+    location: '',
+    description: '',
+
+  })
+
+  const onSubmitHandler = async (e)=>{
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      
+    <div className='px-4 py-10 md:px-10 flex-1'>
+      <Title title="Add New Car" subTitle="Fill in details to list a new car for booking, including pricing, availability, and car specifications."/>
+
+      <form onSubmit={onSubmitHandler} className='flex flex-col gap-5 text-gray-500 text-sm mt-6 max-w-xl'>
+        
+        {/* car image */}
+        <div className='flex items-center gap-2 w-full'>
+          <label htmlFor='car-image' >
+            <img src={image ? URL.createObjectURL(image) : assets.upload_icon} alt=""className='h-14 rounded cursor-pointer ' />
+            <input type="file" id='car-image' accept="image/*" hidden onChange={e=>setImage(e.target.files[0])}/>
+          </label>
+          <p className='text-sm text-gray-500'>Upload a picture of your car</p>
+        </div>
+
+      </form>
     </div>
   )
 }
